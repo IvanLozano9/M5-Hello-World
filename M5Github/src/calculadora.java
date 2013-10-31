d252ec618aaeadefdfce7997a8baf56bb13e810f
IvanLozano9 <ivanlozano1992@gmail.com>
2013-10-31 20:32:39 +0100

    created calculadora.java

--- M5Github/src/calculadora.java
+++ M5Github/src/calculadora.java
@@ -2,7 +2,7 @@
 public class calculadora {
 
 	public static void main(String[] args) {
-		System.out.println("Soc una calculadora simple");
+		System.out.println("Soc una calculadora cientifica");
 
 	}
 
